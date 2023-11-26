fe449acf3ef0c3fc03f5c8f27bc6a90175f903dc
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-11-26 19:59:34 +0530

    find -> get

--- cypress/e2e/sample.cy.tsx
+++ cypress/e2e/sample.cy.tsx
@@ -1,6 +1,6 @@
 describe("sample", () => {
   it("should works", () => {
     cy.visit("https://docs.cypress.io/examples/recipes/");
-    cy.find("h1").should("have.text", "/recipes/i");
+    cy.get("h1").should("have.text", "/recipes/i");
   });
 });
