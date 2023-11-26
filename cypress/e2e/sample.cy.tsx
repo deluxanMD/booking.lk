be6c652518e0d48ce055f89b42a623238afab9e0
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-11-26 20:05:13 +0530

    lets go with actual string

--- cypress/e2e/sample.cy.tsx
+++ cypress/e2e/sample.cy.tsx
@@ -1,6 +1,6 @@
 describe("sample", () => {
   it("should works", () => {
     cy.visit("https://docs.cypress.io/examples/recipes/");
-    cy.get("h1").should("have.text", /recipes/i);
+    cy.get("h1").should("have.text", "Recipes");
   });
 });
