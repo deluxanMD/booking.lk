34a45ae685a73324f2584fdc144487531f560b5e
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-11-27 15:05:20 +0530

    try this

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 /// <reference types="cypress" />
+import "@cypress/code-coverage/support";
+
 Cypress.Commands.add("dataCy", (value) => {
   const escapedValue = value.replace(/\./g, "\\.");
   return cy.get(`[data-cy=${escapedValue}]`);
