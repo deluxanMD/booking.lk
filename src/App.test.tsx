ce73d316525f0e3c2288216e51bc710f95ecf30f
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-12-07 21:47:11 +0530

    my bad

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("app component", () => {
-  render(<App />);
-  const appContainer = screen.getByTestId("App.Container");
-  expect(appContainer).toBeInTheDocument();
+  test("should work", () => {
+    render(<App />);
+    const appContainer = screen.getByTestId("App.Container");
+    expect(appContainer).toBeInTheDocument();
+  });
 });
